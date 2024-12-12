fc4d9714430651504fe8c49ba2a819acb5dd20a7
ConstasJ <constasj@qq.com>
2024-12-12 16:08:22 +0800

    feat: 完成了一些工作

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,3 @@
-import * as core from '@actions/core';
-import * as http from '@actions/http-client';
+import { run } from "./main";
 
-async function main() {
-    const root = core.getInput("root");
-    if (!root.startsWith("http://") || !root.startsWith("https://")) {
-        core.setFailed("root must be a valid URL");
-        return;
-    }
-    const client = new http.HttpClient("http-client")
-    core.info('Testing connection to ' + root);
-    const headers = {
-        "Content-Type": "application/json; charset=utf-8",
-        "X-Requested-With": "XMLHttpRequest"
-    }
-    const key = core.getInput("api-key");
-    const statusRes = await client.get(`${root}/api/overview?apiKey=${key}`, headers);
-    if(JSON.parse(await statusRes.readBody()).status != "200") {
-        core.setFailed("Failed to connect to the server");
-        return;
-    }
-}
-
-main();
+run();
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import * as core from '@actions/core';
+import * as http from '@actions/http-client';
+import * as glob from '@actions/glob';
+
+export async function run() {
+    const root = core.getInput("root");
+    if (!root.startsWith("http://") && !root.startsWith("https://")) {
+        core.setFailed("root must be a valid URL");
+        return;
+    }
+    const client = new http.HttpClient("http-client")
+    core.info('Testing connection to ' + root);
+    const headers = {
+        "Content-Type": "application/json; charset=utf-8",
+        "X-Requested-With": "XMLHttpRequest"
+    }
+    const key = core.getInput("api-key");
+    const statusRes = await client.get(`${root}/api/overview?apikey=${key}`, headers);
+    const status = await statusRes.readBody();
+    if(JSON.parse(status).status != "200") {
+        core.setFailed("Failed to connect to the server");
+        return;
+    }
+    core.info('Connection successful');
+    const globber = await glob.create(core.getInput("source"));
+    const files = globber.glob();
+}
