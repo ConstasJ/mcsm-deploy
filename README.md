8ddae3c65ce9f00a67e77d4a39ec71b78b359614
ConstasJ <constasj@qq.com>
2024-12-12 17:43:11 +0800

    feat: 重新打包Action

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MCSManager Deploy Action
+
 This action will deploy the file you specified to your MCSManager server.
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import path from 'path'
 import fs from 'fs'
 import axios from 'axios'
 import { fileFromPath } from 'formdata-node/file-from-path'
+import { FormData } from 'formdata-node'
 
 export async function run() {
   const root = core.getInput('root')
@@ -37,83 +38,87 @@ export async function run() {
   core.debug(files.join('\n'))
   await Promise.all(
     files.map(async file => {
-        core.info(`find file ${path.relative(process.cwd(), file)}`)
-        core.info(`Uploading file to configured MCSManager application instance...`)
-        const fileName = path.basename(file)
-        const fileListObj = JSON.parse(
-          await (
-            await client.get(
-              `${root}/api/files/list?apikey=${key}&daemonId=${daemonId}&uuid=${appId}&target=${targetPath}&file_name=${fileName}&page=0&page_size=100`,
-              headers
-            )
-          ).readBody()
-        )
-        if ((fileListObj.data.items as any[]).length > 0) {
-          core.info(`File ${fileName} already exists on the server, deleting it...`)
-          const delRes = await client.request(
-            'DELETE',
-            `${root}/api/files?apikey=${key}&daemonId=${daemonId}&uuid=${appId}`,
-            JSON.stringify({
-              targets: [`/${targetPath}/${fileName}`]
-            }),
+      core.info(`find file ${path.relative(process.cwd(), file)}`)
+      core.info(
+        `Uploading file to configured MCSManager application instance...`
+      )
+      const fileName = path.basename(file)
+      const fileListObj = JSON.parse(
+        await (
+          await client.get(
+            `${root}/api/files/list?apikey=${key}&daemonId=${daemonId}&uuid=${appId}&target=${targetPath}&file_name=${fileName}&page=0&page_size=100`,
             headers
           )
-          if (delRes.message.statusCode != 200) {
-            core.setFailed(`Failed to delete file ${targetPath}/${fileName}`)
-            return
-          }
-          core.info(`File ${fileName} deleted successfully`)
-        }
-        core.info(`Getting upload URL for file ${fileName}...`)
-        const uploadArgObj = JSON.parse(
-          await (
-            await client.post(
-              `${root}/api/files/upload?apikey=${key}&daemonId=${daemonId}&uuid=${appId}&upload_dir=${targetPath}&file_name=${fileName}`,
-              ''
-            )
-          ).readBody()
+        ).readBody()
+      )
+      if ((fileListObj.data.items as any[]).length > 0) {
+        core.info(
+          `File ${fileName} already exists on the server, deleting it...`
         )
-        if (uploadArgObj.status != 200) {
-          core.setFailed(`Failed to get upload URL for file ${fileName}`)
+        const delRes = await client.request(
+          'DELETE',
+          `${root}/api/files?apikey=${key}&daemonId=${daemonId}&uuid=${appId}`,
+          JSON.stringify({
+            targets: [`/${targetPath}/${fileName}`]
+          }),
+          headers
+        )
+        if (delRes.message.statusCode != 200) {
+          core.setFailed(`Failed to delete file ${targetPath}/${fileName}`)
           return
         }
-        core.info(`Uploading file ${fileName} to ${uploadArgObj.data.addr}...`)
-        const form = new FormData()
-        form.append('file', await fileFromPath(file))
-        const addr = (() => {
-          if (uploadArgObj.data.addr.startsWith('wss://')) {
-            return uploadArgObj.data.addr.replace('wss://', '')
-          } else {
-            return uploadArgObj.data.addr
-          }
-        })()
-        const uploadRes = await axios.postForm(
-          `https://${addr}/upload/${uploadArgObj.data.password}`,
-          form,
-          {
-            headers: {
-              'Content-Type': 'multipart/form-data',
-              'Content-Length': fs.readFileSync(file).length
-            }
-          }
-        )
-        if (uploadRes.status != 200) {
-          core.setFailed(`Failed to upload file ${fileName}`)
-          return
+        core.info(`File ${fileName} deleted successfully`)
+      }
+      core.info(`Getting upload URL for file ${fileName}...`)
+      const uploadArgObj = JSON.parse(
+        await (
+          await client.post(
+            `${root}/api/files/upload?apikey=${key}&daemonId=${daemonId}&uuid=${appId}&upload_dir=${targetPath}&file_name=${fileName}`,
+            ''
+          )
+        ).readBody()
+      )
+      if (uploadArgObj.status != 200) {
+        core.setFailed(`Failed to get upload URL for file ${fileName}`)
+        return
+      }
+      core.info(`Uploading file ${fileName} to ${uploadArgObj.data.addr}...`)
+      const form = new FormData()
+      form.append('file', await fileFromPath(file))
+      const addr = (() => {
+        if (uploadArgObj.data.addr.startsWith('wss://')) {
+          return uploadArgObj.data.addr.replace('wss://', '')
+        } else {
+          return uploadArgObj.data.addr
         }
-        core.info(`File ${fileName} uploaded successfully`)
-      })
+      })()
+      const uploadRes = await axios.postForm(
+        `https://${addr}/upload/${uploadArgObj.data.password}`,
+        form,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            'Content-Length': fs.readFileSync(file).length
+          }
+        }
+      )
+      if (uploadRes.status != 200) {
+        core.setFailed(`Failed to upload file ${fileName}`)
+        return
+      }
+      core.info(`File ${fileName} uploaded successfully`)
+    })
   )
-  core.info(`Sleep 5 seconds...`);
-  await new Promise(resolve => setTimeout(resolve, 5000));
-  core.info(`Restart application instance...`);
+  core.info(`Sleep 5 seconds...`)
+  await new Promise(resolve => setTimeout(resolve, 5000))
+  core.info(`Restart application instance...`)
   const restartRes = await client.get(
     `${root}/api/protected_instance/restart?apikey=${key}&daemonId=${daemonId}&uuid=${appId}`,
     headers
-  );
+  )
   if (restartRes.message.statusCode != 200) {
-    core.setFailed(`Failed to restart application instance`);
-    return;
+    core.setFailed(`Failed to restart application instance`)
+    return
   }
-  core.info(`Application instance restarted successfully`);
+  core.info(`Application instance restarted successfully`)
 }
