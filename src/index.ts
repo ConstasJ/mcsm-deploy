2328d40f46efa9bb1fae0fc5ab055301437de1eb
ConstasJ <constasj@qq.com>
2024-12-12 09:21:10 +0800

    feat: 完成了验证连接部分

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core';
+import * as http from '@actions/http-client';
 
 async function main() {
     const root = core.getInput("root");
@@ -6,7 +7,18 @@ async function main() {
         core.setFailed("root must be a valid URL");
         return;
     }
-    const url = new URL(root);
+    const client = new http.HttpClient("http-client")
+    core.info('Testing connection to ' + root);
+    const headers = {
+        "Content-Type": "application/json; charset=utf-8",
+        "X-Requested-With": "XMLHttpRequest"
+    }
+    const key = core.getInput("api-key");
+    const statusRes = await client.get(`${root}/api/overview?apiKey=${key}`, headers);
+    if(JSON.parse(await statusRes.readBody()).status != "200") {
+        core.setFailed("Failed to connect to the server");
+        return;
+    }
 }
 
 main();
