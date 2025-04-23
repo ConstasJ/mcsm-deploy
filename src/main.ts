55d8e05c5824bbce0e17f25df19f482f260dfded
ConstasJ <constasj@qq.com>
2025-04-24 05:42:46 +0800

    fix: 优化文件上传失败时的错误处理逻辑

--- src/main.ts
+++ src/main.ts
@@ -33,8 +33,8 @@ export async function run() {
   const globber = await glob.create(core.getInput('source'))
   const files = await globber.glob()
   if (files.length <= 0) {
-    core.setFailed('No file matches the source pattern');
-    return;
+    core.setFailed('No file matches the source pattern')
+    return
   }
   const daemonId = core.getInput('daemon-id')
   const appId = core.getInput('server-id')
@@ -43,72 +43,76 @@ export async function run() {
   await Promise.all(
     files.map(async file => {
       core.info(`find file ${path.relative(process.cwd(), file)}`)
-    core.info(`Uploading file to configured MCSManager application instance...`)
-    const fileName = path.basename(file)
-    core.info(`Getting upload URL for file ${fileName}...`)
-    const uploadArgObj = JSON.parse(
-      await (
-        await client.post(
-          `${root}/api/files/upload?apikey=${key}&daemonId=${daemonId}&uuid=${appId}&upload_dir=${targetPath}&file_name=${fileName}`,
-          ''
-        )
-      ).readBody()
-    )
-    if (uploadArgObj.status != 200) {
-      core.setFailed(`Failed to get upload URL for file ${fileName}`)
-      return
-    }
-    core.info(`Uploading file ${fileName} to ${uploadArgObj.data.addr}...`)
-    const form = new FormData()
-    form.append('file', await fileFromPath(file))
-    const addr = (() => {
-      if (uploadArgObj.data.addr.startsWith('wss://')) {
-        return uploadArgObj.data.addr.replace('wss://', '')
-      } else {
-        return uploadArgObj.data.addr
+      core.info(
+        `Uploading file to configured MCSManager application instance...`
+      )
+      const fileName = path.basename(file)
+      core.info(`Getting upload URL for file ${fileName}...`)
+      const uploadArgObj = JSON.parse(
+        await (
+          await client.post(
+            `${root}/api/files/upload?apikey=${key}&daemonId=${daemonId}&uuid=${appId}&upload_dir=${targetPath}&file_name=${fileName}`,
+            ''
+          )
+        ).readBody()
+      )
+      if (uploadArgObj.status != 200) {
+        core.setFailed(`Failed to get upload URL for file ${fileName}`)
+        return
       }
-    })()
-    const uploadRes = await axios.postForm(
-      `https://${addr}/upload/${uploadArgObj.data.password}?overwrite=true`,
-      form,
-      {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          'Content-Length': fs.readFileSync(file).length
+      const form = new FormData()
+      form.append('file', await fileFromPath(file))
+      const addr = (() => {
+        if (uploadArgObj.data.addr.startsWith('wss://')) {
+          return uploadArgObj.data.addr
+            .replace('wss://', 'https://')
+            .replace(':443', '')
+        } else {
+          return uploadArgObj.data.addr
         }
+      })()
+      core.info(`Uploading file ${fileName} to ${addr}...`)
+      const uploadRes = await axios.postForm(
+        `${addr}/upload/${uploadArgObj.data.password}?overwrite=true`,
+        form,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            'Content-Length': fs.readFileSync(file).length
+          }
+        }
+      )
+      if (uploadRes.status != 200) {
+        core.setFailed(`Failed to upload file ${fileName}`)
+        return
       }
-    )
-    if (uploadRes.status != 200) {
-      core.setFailed(`Failed to upload file ${fileName}`)
-      return
-    }
-    core.info(`File ${fileName} uploaded successfully`)
+      core.info(`File ${fileName} uploaded successfully`)
     })
   )
   core.info(`Restart application instance...`)
-  let restartRes;
+  let restartRes
   try {
     restartRes = await axios.get(
       `${root}/api/protected_instance/restart?apikey=${key}&daemonId=${daemonId}&uuid=${appId}`,
       {
-        headers,
+        headers
       }
     )
   } catch (e: any) {
     if (e instanceof AxiosError) {
       if (e.response) {
-        core.debug(JSON.stringify(e.response.data));
-        core.debug(e.response.status.toString());
-        core.debug(JSON.stringify(e.response.headers));
+        core.debug(JSON.stringify(e.response.data))
+        core.debug(e.response.status.toString())
+        core.debug(JSON.stringify(e.response.headers))
       } else if (e.request) {
-        core.debug(JSON.stringify(e.request));
+        core.debug(JSON.stringify(e.request))
       } else {
-        core.debug(e.message);
+        core.debug(e.message)
       }
-      core.debug(JSON.stringify(e.config) || "");
+      core.debug(JSON.stringify(e.config) || '')
     }
-    core.setFailed("Failed to restart application instance");
-    return;
+    core.setFailed('Failed to restart application instance')
+    return
   }
   if (restartRes.status != 200) {
     core.setFailed(`Failed to restart application instance`)
